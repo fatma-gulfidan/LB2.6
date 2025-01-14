8b335baf084144139ae6c8cf643d81aa860d06d5
fatma gülfidan <nesepalamutlari@icloud.com>
2025-01-14 23:31:53 +0100

    Kullanıcı giriş özelliği eklendi

--- AB2.6/obj/Debug/net8.0/AB2.6.AssemblyInfo.cs
+++ AB2.6/obj/Debug/net8.0/AB2.6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AB2.6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16e498641deb2e2244bd16a245dbeb21cd87763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be254ea796b9fd467ad1c901997b59004f89b73c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AB2.6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AB2.6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
